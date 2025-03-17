5da4484ae8b83b6d64adac680e80cc584fa88c22
DevMunky <126913961+DevMunky@users.noreply.github.com>
2025-03-17 11:54:31 -0400

    * Update build scripts to remove publishing stuff as i dont use it. * Update for 1.21.4 (I thought i already committed this lol.)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     `java-library`
+
+    `maven-publish`
+    signing
+    alias(libs.plugins.nexuspublish)
 }
 
 group = "dev.hollowcube"
@@ -12,7 +16,6 @@ repositories {
 }
 
 dependencies {
-    println(libs)
     compileOnly(libs.minestom)
     testImplementation(libs.minestom)
 
@@ -30,4 +33,74 @@ java {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+nexusPublishing {
+    this.packageGroup.set("dev.hollowcube")
+
+    repositories.sonatype {
+        nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+        snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+        if (System.getenv("SONATYPE_USERNAME") != null) {
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
+}
+
+publishing.publications.create<MavenPublication>("maven") {
+    groupId = "dev.hollowcube"
+    artifactId = "schem"
+    version = project.version.toString()
+
+    from(project.components["java"])
+
+    pom {
+        name.set(artifactId)
+        description.set(project.description)
+        url.set("https://github.com/hollow-cube/schem")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://github.com/hollow-cube/schem/blob/main/LICENSE")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("mworzala")
+                name.set("Matt Worzala")
+                email.set("matt@hollowcube.dev")
+            }
+        }
+
+        issueManagement {
+            system.set("GitHub")
+            url.set("https://github.com/hollow-cube/schem/issues")
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/hollow-cube/schem.git")
+            developerConnection.set("scm:git:git@github.com:hollow-cube/schem.git")
+            url.set("https://github.com/hollow-cube/schem")
+            tag.set(System.getenv("TAG_VERSION") ?: "HEAD")
+        }
+
+        ciManagement {
+            system.set("Github Actions")
+            url.set("https://github.com/hollow-cube/schem/actions")
+        }
+    }
+}
+
+signing {
+    isRequired = System.getenv("CI") != null
+
+    val privateKey = System.getenv("GPG_PRIVATE_KEY")
+    val keyPassphrase = System.getenv()["GPG_PASSPHRASE"]
+    useInMemoryPgpKeys(privateKey, keyPassphrase)
+
+    sign(publishing.publications)
 }
