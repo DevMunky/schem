20a9bf786ba9b1ae9e6d0c1b3ec7af2aa948ec4b
DevMunky <126913961+DevMunky@users.noreply.github.com>
2025-05-07 16:04:57 -0400

    Added unit testing capabilities, enemy scripting is fully working - requires GraalVM, and added new RenderDispatcher api to replace all the various unrelated rendering solutions.
    * Unit testing added to project and will be utilized as time goes on.
    
    * Scripting now tested and working. Exception handling refined and more work is needed in that regard. More context needs to be added as well. A capability to create projectiles of some sort needs to be added.
    
    * Powerful rendering api has been added. Such api allows any renderer to create a RenderHandle, which has a dedicated coroutine scope ultimately owned by the RenderDispatcher (may own its own CoroutineDispatcher at some point, but currently uses Dispatchers.Default). This allows efficient resource and low performance costs even with complex operations that span a long duration. A RenderRequest is made, which populates a map of keys called a RenderContext akin to a CoroutineContext. This context is used to deduce state, and a renderer may throw an exception when rendering if a certain state is not present, such as an initial position. A renderer is required to make reasonable effort to perform the operation without a given key. The StableRenderKey makes this easier to implement. There is also the ability for a Renderer to get an updated value on the returned RenderHandle upon invocations through a property delegate, or even run a callback upon a structural change to the RenderHandle's RenderContext held within. A renderer is always a required value, and this is reinforced with the static constructor used to create a RenderRequest, RenderRequest.with(RendererKey<T : Renderer>).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../Tenebris/libs.versions.toml"))
+        }
+    }
+}
+
 rootProject.name = "schem"
